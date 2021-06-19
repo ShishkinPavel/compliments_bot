617a04b037349408c2734fe60b95de29667b1908
Pavel Shishkin <41304680+ShishkinPavel@users.noreply.github.com>
2021-06-19 23:26:51 +0200

    delete some spaghetti

--- complimentsbot.py
+++ complimentsbot.py
@@ -7,7 +7,6 @@ from threading import Timer
 BOT = TeleBot("BOT_TOKEN")
 WEBSITE = 'http://kompli.me/komplimenty-devushke'
 
-
 def send_compliment():
     Timer(28800.0, send_compliment).start()
     BOT.send_message("Girlfriend_id", get_random_compliment())
@@ -16,14 +15,11 @@ def send_compliment():
 def get_random_compliment():
     tags_of_page_numbers = BeautifulSoup(get(WEBSITE).text, 'html.parser').select('div.nav-links a')
     count_of_page = tags_of_page_numbers[-2].text
-
     random_page_number = str(randint(1, int(count_of_page)))
     webpage = get(WEBSITE + '/page'+ random_page_number).text
-
-    tags = BeautifulSoup(webpage, 'html.parser').find_all('div', {"class": "post-card__title"})
-    compliments = [i.text for i in tags]
-
+    tags_of_compliments = BeautifulSoup(webpage, 'html.parser').find_all('div', {"class": "post-card__title"})
+    compliments = [tag.text for tag in tags_of_compliments]
     return choice(compliments)
 
 
-print(get_random_compliment())
+send_compliment()
