fea74a69da0b5f0e182f9a2a36fe478c442848b7
Pavel Shishkin <41304680+ShishkinPavel@users.noreply.github.com>
2021-06-29 12:48:19 +0200

    Сleaned up the code a little

--- complimentsbot.py
+++ complimentsbot.py
@@ -1,8 +1,6 @@
-import random
 import time
-
 from telebot import TeleBot
-from random import choice
+from random import choice, randint
 from requests import get
 from bs4 import BeautifulSoup
 import sqlite3
@@ -15,24 +13,25 @@ CURSOR = CONNECT.cursor()
 
 def send_compliment():
     compliment = get_random_compliment()
-    while compliment is not None:
+    while compliment:
         BOT.send_message("Girlfriend ID", compliment)
         compliment = get_random_compliment()
-        sleep = random.randint(20000, 30000)
+        sleep = randint(25000, 33000)
         time.sleep(sleep)
+    BOT.send_message("Girlfriend ID", "Комплименты кончились")
 
 
 def get_random_compliment():
     database_create()
     exist = CURSOR.execute('''SELECT * FROM compliments''').fetchall()
+
     if not exist:
-        tags_of_page_numbers = BeautifulSoup(get(WEBSITE).text, 'html.parser').select('div.nav-links a')
-        count_of_page = int(tags_of_page_numbers[-2].text)
-        xx = get_all_compliments(count_of_page)
-        database_append(xx)
+        get_all_compliments()
+
     compliments = CURSOR.execute('''SELECT * FROM compliments WHERE used = 'No';''').fetchall()
     if not compliments:
         return None
+
     compliment = choice(compliments)
     compliment_index = int(compliment[0])
     database_update(compliment_index)
@@ -40,16 +39,16 @@ def get_random_compliment():
     return compliment_text
 
 
-def get_all_compliments(count_of_page):
-    all_compliments = []
+def get_all_compliments():
+    tags_of_page_numbers = BeautifulSoup(get(WEBSITE).text, 'html.parser').select('div.nav-links a')
+    count_of_page = int(tags_of_page_numbers[-2].text)
     index = 0
     for page in range(1, count_of_page + 1):
         webpage = get(WEBSITE + '/page' + str(page)).text
         tags_of_compliments = BeautifulSoup(webpage, 'html.parser').find_all('div', {"class": "post-card__title"})
-        for j in tags_of_compliments:
-            all_compliments.append((index, j.text, 'No'))
+        for tag in tags_of_compliments:
+            database_append((index, tag.text, 'No'))
             index += 1
-    return all_compliments
 
 
 def database_create():
@@ -61,16 +60,15 @@ def database_create():
     CONNECT.commit()
 
 
-def database_append(compliments_tuples):
-    CURSOR.executemany('''INSERT INTO compliments VALUES(?, ?, ?);''', compliments_tuples)
+def database_append(compliments_tuple):
+    CURSOR.execute('''INSERT INTO compliments VALUES(?, ?, ?);''', compliments_tuple)
     CONNECT.commit()
 
 
 def database_update(compliment_id):
-    sql = '''UPDATE compliments 
+    CURSOR.execute('''UPDATE compliments 
             SET used = 'Yes'
-            WHERE compliment_id = (?)'''
-    CURSOR.execute(sql, (compliment_id,))
+            WHERE compliment_id = (?)''', (compliment_id,))
     CONNECT.commit()
 
 
